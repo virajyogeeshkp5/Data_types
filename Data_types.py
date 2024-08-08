a8a3de7b3a298083a4fd6e5e464080c69b083fdf
YOGEESH K P <yogeesh793@gmail.com>
2024-08-08 23:35:30 +0530

    Add files via upload

---
+++ Data_types.py
@@ -0,0 +1,62 @@
+#int
+print(231564654+2)
+
+print(20)
+
+print(0b10)
+
+print(0B10)
+
+print(0X20)
+
+a=10
+print(a)
+
+# to verify type of function
+
+print(type(10))
+
+a=11
+print(type(a))
+
+# Float
+
+y=2.8
+print(y)
+
+print(type(y))
+
+print(type(0.4))
+
+x=35e3
+y=12E4
+z=-87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Boolean
+
+print(type(True))
+
+print(type(False))
+
+# String
+
+print("hello")
+
+print(type("hello"))
+
+print('a \n b')
+
+print('a \t b')
+
+# List
+
+list = [1,5,6,4,2,4,[5,6,5,5]]
+print(list)
+
+x = []
+print(x)
+
+
